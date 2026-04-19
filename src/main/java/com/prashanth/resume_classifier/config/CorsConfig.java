8e6b1bfd5a40f23115233f6cb52741f26e607ddf
prashanth <prashanth.art2001@gmail.com>
2026-04-19 11:15:59 +0530

    cors

--- src/main/java/com/prashanth/resume_classifier/config/CorsConfig.java
+++ src/main/java/com/prashanth/resume_classifier/config/CorsConfig.java
@@ -1,11 +1,17 @@
 package com.prashanth.resume_classifier.config;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("CORS CONFIG LOADED");
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
